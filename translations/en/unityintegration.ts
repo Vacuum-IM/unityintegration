15c5d43a99a8b896a6c275865e8a40d4cef7fae5
Sergey Potapov <potapov.s.a@gmail.com>
2012-07-14 14:25:18 +0400

    Добавлена английская локализация для удобства перевода в Transifex.

--- translations/en/unityintegration.ts
+++ translations/en/unityintegration.ts
@@ -1,43 +1,43 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="de" sourcelanguage="en">
+<TS version="2.0" sourcelanguage="en">
 <context>
     <name>UnityIntegration</name>
     <message>
         <source>Unity Integration</source>
-        <translation type="unfinished"></translation>
+        <translation>Unity Integration</translation>
     </message>
     <message>
         <source>Provides integration with Dash panel Unity</source>
-        <translation type="unfinished"></translation>
+        <translation>Provides integration with Dash panel Unity</translation>
     </message>
     <message>
         <source>Show roster</source>
-        <translation>Roster anzeigen</translation>
+        <translation>Show roster</translation>
     </message>
     <message>
         <source>Show all hidden conferences</source>
-        <translation type="unfinished"></translation>
+        <translation>Show all hidden conferences</translation>
     </message>
     <message>
         <source>File Transfers</source>
-        <translation>Dateiübertragungen</translation>
-    </message>
-    <message>
-        <source>Setup plugins</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Change Profile</source>
-        <translation type="unfinished"></translation>
+        <translation>File Transfers</translation>
     </message>
     <message>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Options</translation>
+    </message>
+    <message>
+        <source>Setup plugins</source>
+        <translation>Setup plugins</translation>
+    </message>
+    <message>
+        <source>Change Profile</source>
+        <translation>Change Profile</translation>
     </message>
     <message>
         <source>Quit</source>
-        <translation>Schließen</translation>
+        <translation>Quit</translation>
     </message>
 </context>
 </TS>
